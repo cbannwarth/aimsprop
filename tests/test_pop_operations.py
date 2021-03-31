a40d2b6f8918611741594eb0db9e54774d3b17ee
Alice Walker <awalker@chem.wayne.edu>
2021-03-31 16:11:15 -0700

    Added more tests

--- tests/test_pop_operations.py
+++ tests/test_pop_operations.py
@@ -8,4 +8,4 @@ def test_compute_population(trajectory):
     # 1. Compute the populations for states.
     # Test if the computed population at time 25 is correct.
     pop = ai.compute_population(trajectory)
-    assert pop[1][24] == pt.approx(0.44406572, rel=1e-7)
+    assert pop[1][24] == pt.approx(0.441434205, rel=1e-7)
